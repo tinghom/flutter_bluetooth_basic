6cef0220e6d20e49997c7142e047910129c1ee5c
Tim <tinghom@gmail.com>
2025-05-08 16:57:11 +0800

    add import

--- android/src/main/java/com/tablemi/flutter_bluetooth_basic/DeviceConnFactoryManager.java
+++ android/src/main/java/com/tablemi/flutter_bluetooth_basic/DeviceConnFactoryManager.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import com.gprinter.io.*;
